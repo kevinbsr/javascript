3e91c535833f7a8129648236ecf7309718838007
Kevinbsr <kb.kevinbenevides@gmail.com>
2024-01-13 00:00:07 -0300

    Add functions exercises

---
+++ functions.js
@@ -0,0 +1,18 @@
+/* function teste() {
+  console.log('Hello, world!');
+}
+
+teste() */
+
+/* function sayMyName(name) {
+  console.log(`my name is ${name}!`);
+}
+
+sayMyName('Kevin') */
+
+function quadrado(valor) {
+  return valor*valor
+}
+
+const quadradoDeDez = quadrado(10)
+console.log(quadradoDeDez);
